8def808dd3fa6a4ef0089978e9c0b7839f5485b7
NWOM <98202572+NWOM@users.noreply.github.com>
2024-03-13 19:56:20 +0530

    Update CinemasApplication.java

--- cinemas/src/main/java/com/movies/cinemas/CinemasApplication.java
+++ cinemas/src/main/java/com/movies/cinemas/CinemasApplication.java
@@ -2,6 +2,9 @@ package com.movies.cinemas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,5 +14,17 @@ public class CinemasApplication {
 		SpringApplication.run(CinemasApplication.class, args);
 
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
